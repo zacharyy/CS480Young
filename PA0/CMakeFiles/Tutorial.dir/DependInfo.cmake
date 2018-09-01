0ef6fa0419cf5338afc73a9a2b0275bf8234d163
Z <zachidk@gmail.com>
2018-09-01 13:09:02 -0700

    PA1

---
+++ PA0/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/src/camera.cpp" "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/src/engine.cpp" "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/src/graphics.cpp" "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/src/main.cpp" "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/src/object.cpp" "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/src/shader.cpp" "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/src/window.cpp" "/home/zacharyy/Desktop/CS480/computer-graphics/PA0/CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNIX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
